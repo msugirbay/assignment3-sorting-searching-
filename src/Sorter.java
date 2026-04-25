33f059ef1cb9d4cbc5bf1eab36242833834c4186
msugirbay <msugirbay@inbox.ru>
2026-04-25 20:26:03 +0500

    Create Sorter.java

---
+++ src/Sorter.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+public class Sorter {
+
+    public void basicSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void advancedSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public void printArray(int[] arr) {
+        for (int val : arr) System.out.print(val + " ");
+        System.out.println();
+    }
+
+    public int[] generateRandomArray(int size) {
+        Random r = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = r.nextInt(1000);
+        }
+        return arr;
+    }
+}
